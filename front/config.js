7f2f4abd4d0a0921b833b4c14431078bcb9dafd3
Анияр Дурмагамбетова <durmagambetovaa4@gmail.com>
2022-09-25 00:19:18 +0600

    swift

---
+++ front/config.js
@@ -0,0 +1,3 @@
+var config = {
+    accessToken: 'pk.eyJ1IjoiYW5peWFyYXNzZXQiLCJhIjoiY2wyN2Vzdm95MDZmcDNrcm04ZGRnYWRwaiJ9.RSiLEjFoYH89SiXomr3LpA'
+};
---
+++ swift/ARKitCoreLocationEngine.swift
@@ -0,0 +1,90 @@
+
+import Foundation
+import CoreLocation
+import SceneKit
+import ARKit
+
+protocol ARKitCoreLocationEngine {
+    /// Converts geo coordinate to 3D position
+    ///
+    /// - Parameter coordinate: position on earth
+    /// - Returns: position on 3D scene with y = 0
+    func convert(coordinate: CLLocationCoordinate2D) -> SCNVector3?
+    func userLocationEstimate() -> SceneLocationEstimate?
+}
+
+class ARKitCoreLocationEngineImpl: NSObject, ARKitCoreLocationEngine {
+    let barometrLabel: UILabel
+
+    init(view: SCNView, locationManager: LocationManager, locationEstimatesHolder: LocationEstimatesHolder, barometrLabel: UILabel) {
+        self.scnView = view
+        self.locationManager = locationManager
+        self.locationEstimatesHolder = locationEstimatesHolder
+        self.barometrLabel = barometrLabel
+        super.init()
+
+        filterLocationEstimatesAction = TimerAction(timeInterval: 3.0, repeats: true) { [weak self] in
+            self?.filterLocationEstimates()
+        }
+        locationManager.addListener(self)
+    }
+
+    func userLocationEstimate() -> SceneLocationEstimate? {
+        guard let bestEstimate = locationEstimatesHolder.bestLocationEstimate else { return nil }
+        guard let position = currentScenePosition() else { return nil }
+        let correctLocation = bestEstimate.translatedLocation(to: position)
+        return SceneLocationEstimate(location: correctLocation, position: position)
+    }
+
+    func convert(coordinate: CLLocationCoordinate2D) -> SCNVector3? {
+        guard let anchorEstimate = locationEstimatesHolder.bestLocationEstimate else { return nil }
+        let location = anchorEstimate.location
+        let position = anchorEstimate.position
+        let translation = location.coordinate.translation(toCoordinate: coordinate)
+        return SCNVector3(
+            x: position.x + Float(translation.longitudeTranslation),
+            y: 0.0,
+            z: position.z - Float(translation.latitudeTranslation)
+        )
+    }
+
+    private let scnView: SCNView
+    private let locationManager: LocationManager
+    private let locationEstimatesHolder: LocationEstimatesHolder
+    private var filterLocationEstimatesAction: TimerAction? = nil
+}
+
+extension ARKitCoreLocationEngineImpl {
+    private func currentScenePosition() -> SCNVector3? {
+        return scnView.pointOfView?.worldPosition
+    }
+
+    private func currentEulerAngles() -> SCNVector3? {
+        return scnView.pointOfView?.eulerAngles
+    }
+
+    /// Filters locationEstimates
+    func filterLocationEstimates() {
+        guard let positionOnScene = currentScenePosition() else { return }
+        let currentPoint = CGPoint(position: positionOnScene)
+        locationEstimatesHolder.filter {
+            let point = CGPoint(position: $0.position)
+            return currentPoint.radiusContainsPoint(radius: Constants.sceneRadiusLimit, point: point)
+        }
+    }
+}
+
+extension ARKitCoreLocationEngineImpl: LocationManagerListener {
+    func onAuthorizationStatusUpdate(_ authorizationStatus: CLAuthorizationStatus) {
+        
+    }
+
+    func onLocationUpdate(_ location: CLLocation) {
+        barometrLabel.text = "Level: \(Int((location.altitude - 349)/3)+1)/\(Int(location.altitude))m"
+        
+        //barometrLabel.text = "Level: \(Int(location.altitude))"
+        guard let positionOnScene = currentScenePosition() else { return }
+        let newLocationEstimate = SceneLocationEstimate(location: location, position: positionOnScene)
+        locationEstimatesHolder.add(newLocationEstimate)
+    }
+}
